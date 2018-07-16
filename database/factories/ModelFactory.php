4f3338f995c6b67afdf3cd9b936f067815e0ea5c
zhucong <tniub.cc@gmail.com>
2018-07-16 02:14:32 +0000

    add Models

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('123456'),
+        'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Models\Thread::class,function ($faker){
+    return [
+        'user_id' => function () {
+            return factory('App\Models\User')->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+    ];
+});
+
+$factory->define(App\Models\Reply::class,function ($faker){
+    return [
+        'thread_id' => function () {
+            return factory('App\Models\Thread')->create()->id;
+        },
+        'user_id' => function () {
+            return factory('App\Models\User')->create()->id;
+        },
+        'body' => $faker->paragraph,
+    ];
+});
